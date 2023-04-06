801ae7d1a6fed97501fa6cc3e839e9a0969c64a6
HughMorris01 <gfarrell2@Live.com>
2023-04-06 09:52:37 -0400

    Refactor, Updates

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,6 @@
-
 const progress = document.querySelector('.progress-bars-wrapper');
 const percentBars = document.querySelectorAll('.progress-percent');
-const percentages = [80, 95, 90, 75, 75, 35, 45, 40];
-
+const percentages = [80, 95, 90, 75, 75, 70, 70, 70];
 
 // Navbar
 const navbar = document.querySelector('.navbar');
@@ -12,41 +10,42 @@ const navbarOffsetTop = navbar.offsetTop;
 const section2Offset = document.querySelector('.section-2').offsetTop;
 const section3Offset = document.querySelector('.section-3').offsetTop;
 
-window.addEventListener('scroll', ()=>{
-    mainFn()
+window.addEventListener('scroll', () => {
+  mainFn();
 });
 
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
 
-const mainFn = ()=>{
-    if(window.pageYOffset >= navbarOffsetTop){
-        navbar.classList.add('sticky');
-    } else {
-        navbar.classList.remove('sticky')
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 200) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove('change');
+        navbarLink.setAttribute('pointer-events', 'none');
+      });
+      navbarLinks[i].classList.add('change');
     }
+  });
 
-    
-    sections.forEach((section, i) => {
-
-        if(window.pageYOffset >= (section.offsetTop-200)) {
-            navbarLinks.forEach(navbarLink =>{
-                navbarLink.classList.remove('change');
-                navbarLink.setAttribute("pointer-events", "none");
-            })
-            navbarLinks[i].classList.add('change');
-        }
+  if (
+    window.pageYOffset >= section2Offset - 100 &&
+    window.pageYOffset < section3Offset - 100
+  ) {
+    percentBars.forEach((percentBar, i) => {
+      percentBar.style.width = `${percentages[i]}%`;
+      percentBar.previousElementSibling.firstElementChild.textContent =
+        percentages[i];
     });
-
-    if(window.pageYOffset >= section2Offset - 100 && window.pageYOffset < section3Offset - 100) {
-        percentBars.forEach((percentBar, i) =>{
-            percentBar.style.width = `${percentages[i]}%`;
-            percentBar.previousElementSibling.firstElementChild.textContent = percentages[i];
-        });
-    } else {
-        percentBars.forEach((percentBar, i) =>{
-            percentBar.style.width = 0;
-        });
-    }
-}
+  } else {
+    percentBars.forEach((percentBar, i) => {
+      percentBar.style.width = 0;
+    });
+  }
+};
 // End of Navbar
 
 // Contact Form
@@ -55,95 +54,110 @@ const senderName = document.getElementById('name');
 const senderEmail = document.getElementById('email');
 const senderSubject = document.getElementById('subject');
 const senderMessage = document.getElementById('message');
-const inputs = document.querySelectorAll(".form-input");
-const submitButton = document.getElementById('submit-btn')
+const inputs = document.querySelectorAll('.form-input');
+const submitButton = document.getElementById('submit-btn');
 let sentMessages = 0;
 let errorMessages = 0;
 
-contactForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    if(validate() === false){
-        return;
+  if (validate() === false) {
+    return;
+  }
+
+  let formData = {
+    name: senderName.value,
+    email: senderEmail.value,
+    subject: senderSubject.value,
+    message: senderMessage.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/', true);
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function () {
+    if (xhr.responseText === 'success') {
+      if (sentMessages === 0) {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>Thank you, I've received your message.</h1>";
+      } else if (sentMessages === 1) {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>Thank you. I've received your message, again. &#128513;</h1>";
+      } else {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>Three is the limit, please try again later &#128526;</h1>";
+        senderName.disabled = true;
+        senderEmail.disabled = true;
+        senderSubject.disabled = true;
+        senderMessage.disabled = true;
+        submitButton.disabled = true;
+        submitButton.classList.add('disable-btn');
+      }
+      senderName.value = '';
+      senderEmail.value = '';
+      senderSubject.value = '';
+      senderMessage.value = '';
+
+      sentMessages++;
+    } else {
+      if (errorMessages === 0) {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>Uh, oh. Something went wrong.......</h1>";
+        errorMessages++;
+      } else {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>There must be an issue, please try again later.</h1>";
+      }
+      senderName.value = '';
+      senderEmail.value = '';
+      senderSubject.value = '';
+      senderMessage.value = '';
     }
-
-    let formData = {
-        name: senderName.value,
-        email: senderEmail.value,
-        subject: senderSubject.value,
-        message: senderMessage.value
-    }
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/', true)
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function (){ 
-        if(xhr.responseText === 'success'){
-            if(sentMessages === 0){
-                document.querySelector(".success").innerHTML = "<h2 class='message-sent'>Thank you, I've received your message.</h1>";
-            }else if (sentMessages === 1){
-                document.querySelector(".success").innerHTML = "<h2 class='message-sent'>Thank you. I've received your message, again. &#128513;</h1>"; 
-            } else {
-                document.querySelector(".success").innerHTML = "<h2 class='message-sent'>Three is the limit, please try again later &#128526;</h1>";
-                senderName.disabled = true;
-                senderEmail.disabled = true;
-                senderSubject.disabled = true;
-                senderMessage.disabled = true;
-                submitButton.disabled = true;
-                submitButton.classList.add('disable-btn');
-            }
-            senderName.value = '';
-            senderEmail.value = '';
-            senderSubject.value = '';
-            senderMessage.value = '';
-
-            sentMessages++;
-        } else {
-            if(errorMessages === 0){
-                document.querySelector(".success").innerHTML = "<h2 class='message-sent'>Uh, oh. Something went wrong.......</h1>";
-                errorMessages++;
-            }else{
-                document.querySelector(".success").innerHTML = "<h2 class='message-sent'>There must be an issue, please try again later.</h1>"; 
-            }
-            senderName.value = '';
-            senderEmail.value = '';
-            senderSubject.value = '';
-            senderMessage.value = '';
-        }
-    }
-    xhr.send(JSON.stringify(formData));
+  };
+  xhr.send(JSON.stringify(formData));
 });
 
-const validate = ()=> {
-    if(senderName.value === ""){
-        document.getElementById('name-validation').innerHTML = "Please enter your name before submitting";
-        senderName.classList.add('validation-failed');
-        return false;
-    }
-    if(senderEmail.value === ""){
-        document.getElementById('email-validation').innerHTML = "Please enter your email before submitting"
-        senderEmail.classList.add('validation-failed');
-        return false;
-    }
-    if(senderSubject.value === ""){
-        document.getElementById('subject-validation').innerHTML = "Please enter a subject before submitting"
-        senderSubject.classList.add('validation-failed');
-        return false;
-    }
-    if(senderMessage.value === ""){
-        document.getElementById('message-validation').innerHTML = "Please enter a message before submitting"
-        senderMessage.classList.add('validation-failed');
-        return false;
-    }
-}
+const validate = () => {
+  if (senderName.value === '') {
+    document.getElementById('name-validation').innerHTML =
+      'Please enter your name before submitting';
+    senderName.classList.add('validation-failed');
+    return false;
+  }
+  if (senderEmail.value === '') {
+    document.getElementById('email-validation').innerHTML =
+      'Please enter your email before submitting';
+    senderEmail.classList.add('validation-failed');
+    return false;
+  }
+  if (senderSubject.value === '') {
+    document.getElementById('subject-validation').innerHTML =
+      'Please enter a subject before submitting';
+    senderSubject.classList.add('validation-failed');
+    return false;
+  }
+  if (senderMessage.value === '') {
+    document.getElementById('message-validation').innerHTML =
+      'A message must be entered before submitting';
+    senderMessage.classList.add('validation-failed');
+    return false;
+  }
+};
 
-inputs.forEach((input) =>{
-    input.addEventListener('input', (e)=>{
-        input.classList.remove('validation-failed');
-        input.nextElementSibling.innerHTML = "";
-    })
-})
+inputs.forEach((input) => {
+  input.addEventListener('input', (e) => {
+    input.classList.remove('validation-failed');
+    input.nextElementSibling.innerHTML = '';
+  });
+});
 // End of Contact Form
+
+// Set copyright year
+const element = document.querySelector('#copyrightYear');
+const year = new Date().getFullYear();
+element.innerHTML = year;
+// End of set copyright year
 
 // window.addEventListener('resize', ()=>{
 //     window.location.reload();
--- server.js
+++ server.js
@@ -1,68 +1,68 @@
 //jshint esversion: 6
-require('dotenv').config({path: __dirname + '/.env'})
-const express = require("express");
-const bodyParser = require("body-parser");
+require('dotenv').config({ path: __dirname + '/.env' });
+const express = require('express');
+const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/views/index.html")
-})
+  res.sendFile(__dirname + '/public/views/index.html');
+});
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    const transporter = nodemailer.createTransport({
-        host: "smtp.zoho.com",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            user: process.env.ZOHO_USERNAME,
-            pass: process.env.ZOHO_PASS,
-        }
-    })
-  
-    const mailOptions = {
-        from: process.env.ZOHO_GREG,
-        to: process.env.MY_EMAIL,
-        subject: `Message from: ${req.body.name} <${req.body.email}> : ${req.body.subject}`,
-        text: req.body.message
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.zoho.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.ZOHO_USERNAME,
+      pass: process.env.ZOHO_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.ZOHO_GREG,
+    to: process.env.MY_EMAIL,
+    subject: `Message from: ${req.body.name} <${req.body.email}> : ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  const confirmationOptions = {
+    from: process.env.ZOHO_NO_REPLY,
+    to: req.body.email,
+    subject: `Message Receipt Confirmation`,
+    text: "I received the message that you sent through my website. Thank you, I'll be in touch.",
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+      res.send('error');
+    } else {
+      res.send('success');
+      console.log('successfully sent email');
     }
+  });
 
-    const confirmationOptions = {
-        from: process.env.ZOHO_NO_REPLY,
-        to: req.body.email,
-        subject: `Message Receipt Confirmation`,
-        text: "I received the message that you sent through my website. Thank you, I'll be in touch."
+  transporter.sendMail(confirmationOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+      res.send('error');
+    } else {
+      res.send('success');
+      console.log('successfully sent email');
     }
-
-    transporter.sendMail(mailOptions, (err, info)=>{
-        if(err){
-            console.log(err);
-            res.send('error');
-        } else {
-            res.send('success');
-            console.log('successfully sent email');
-        }
-    })
-
-    transporter.sendMail(confirmationOptions, (err, info)=>{
-        if(err){
-            console.log(err);
-            res.send('error');
-        } else {
-            res.send('success');
-            console.log('successfully sent email');
-        }
-    })
-})
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}.`)
-})
+  console.log(`Server is listening on port ${port}.`);
+});
