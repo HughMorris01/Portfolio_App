e5e34ba26b85e8cc88d6263778dac091a234da8d
Greg Farrell <gfarrell2@live.com>
2023-04-21 11:41:00 -0400

    testing git

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  console.log(req.body);
+//   console.log(req.body);
 
   const transporter = nodemailer.createTransport({
     host: 'smtp.zoho.com',
